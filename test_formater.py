41248694fcac6d99866f091ef8c8cafd6df97e93
Miguelo Ruiz <red2000ace@gmail.com>
2017-11-07 19:28:36 -0600

    Final Test

--- test_formater.py
+++ test_formater.py
@@ -3,6 +3,10 @@ from main import Formater
 import sys
 sys.tracebacklimit = 0
 
+# python -m unittest <file-name>
+# py -m unittest <file-name>
+# python -m unittest discover <test_directory>
+
 
 class TestFormater(TestCase):
     def setUp(self):
@@ -26,16 +30,33 @@ class TestFormater(TestCase):
         self.assertIsInstance(Formater.clean_number('58, 710, 520'), int, msg=msg)
 
     def test_clean_floats(self):
-        pass
+        """-- Test Clean Floats"""
+        msg = "The numerical value for floating is not being returned"
+        self.assertEqual(Formater.clean_number('16,070.45'), float(16070.45), msg=msg)
+        self.assertEqual(Formater.clean_number('58, 710.520'), float(58710.520), msg=msg)
+        self.assertEqual(Formater.clean_number('16.5'), float(16.5), msg=msg)
 
     def test_correct_float_cast(self):
-        pass
+        """-- Test Floats Cast"""
+        msg = "The correct value for Floats is not returned"
+        self.assertTrue(type(Formater.clean_number('16,070.45')) is float, msg=msg)
+        self.assertTrue(type(Formater.clean_number('58, 710.520')) is float, msg=msg)
+        self.assertTrue(type(Formater.clean_number('16.5')) is float, msg=msg)
 
     def test_comma_afther_dot(self):
-        pass
-
-    def test_multiple_dots(self):
-        pass
+        """-- Test Commas afther dot"""
+        msg = "When there are commas after the decimal point, return None.    Example: 13.15,31"
+        self.assertEqual(Formater.clean_number('7,000.00,00'), None, msg=msg)
+        self.assertEqual(Formater.clean_number('13.15,31'), None, msg=msg)
 
     def test_no_valid_entrys(self):
-        pass
+        """-- Test No Valid Entrys"""
+        msg = "None must be returned for entries that do not comply with the pattern"
+        self.assertEqual(Formater.clean_number('#166'), None, msg=msg)
+        self.assertEqual(Formater.clean_number('hola'), None, msg=msg)
+        self.assertEqual(Formater.clean_number('------'), None, msg=msg)
+
+    def test_multiple_dots(self):
+        """-- Test Multiples dots"""
+        msg = "None should be returned if there are more than 2 decimal points"
+        self.assertEqual(Formater.clean_number('166.66.66'), None, msg=msg)
